3958bfd13bf878ccf345f30b1945c6f6670cb0fd
joshua Oyewole <joshpp2013@gmail.com>
2025-11-18 11:48:23 +0100

    add carousel data instead of making an API call

--- app/pages-sections/market-1/section-1/section-1.jsx
+++ app/pages-sections/market-1/section-1/section-1.jsx
@@ -3,16 +3,22 @@ import CarouselCard1 from "../../../components/carousel-cards/carousel-card-1";
 
 // LOCAL CUSTOM COMPONENT
 import CarouselBanner from "./carousel-banner";
-
+import carouselData from "../../../data/market-1/data";
 // API FUNCTIONS
 
 export default async function Section1() {
+  /* 
   const response = await fetch("http://localhost:5001/api/landing-page", {
     cache: "force-cache",
   });
   const res = await response.json();
 
-  const carousels = res.data.carousels;
+  const carouselss = res.data.carousels; 
+console.log("Carousel Data:", carouselss); 
+*/
+  const carousels = carouselData.carouselData;
+
+  if (!carousels || carousels.length === 0) return null;
 
   return (
     <Container>
